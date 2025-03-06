9467b51ca2cc95e062b8bb0d2c9ce14629dba37e
IDO-FV <673ianfrancovazquez@gmail.com>
2025-03-06 14:17:22 -0600

    Create ejercicio2.c

---
+++ ejercicio2.c
@@ -0,0 +1,21 @@
+//Franco Vazquez Ian Javier
+//Ejercicio 2
+//practica 4 
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int a=3;
+    int b=4;
+    if (a>b)
+    {
+        printf("a es mayor \n");
+    }
+    else
+    {
+        printf("b es mayor o igual a a \n");
+    }
+    return 0;
+}
