ec5a16a14202d350f0613121eeadac0173ea453e
IDO-FV <673ianfrancovazquez@gmail.com>
2025-03-06 13:59:18 -0600

    Update ejercicio1.c

--- ejercicio1.c
+++ ejercicio1.c
@@ -1,1 +1,17 @@
+//Franco Vazquez Ian Javier
+//Ejercicio 1
+//practica 4 
 
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int a=3;
+    int b=2;
+    if (a>b)
+    {
+        printf("a es mayor \n");
+    }
+    return 0;
+}
